fa074df88804b5df65086a4725714e789c7346a2
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-09-23 01:25:56 +0600

    Added logic to generate an array with objects for cells.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import './App.css';
+import {useState} from "react";
+
+const createItems = () => {
+    const items = Array.from({ length: 36 }, () => ({
+        hasItem: false,
+        clicked: false,
+    }));
+
+    const randomIndex = Math.floor(Math.random() * 36);
+    items[randomIndex].hasItem = true;
+
+    return items;
+};
 
 const App = () => {
+    const [items, setItems] = useState(createItems());
+
     return (
         <div className="App"></div>
     );
